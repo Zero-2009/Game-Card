b4d9085b7a157bf3e56678f6f547f39d449278a1
zeritx <luisfabriziosarachaga@gmail.com>
2025-05-07 22:48:24 +0200

    Realizing the part of the game

---
+++ js/Generate-Game.js
@@ -0,0 +1,7 @@
+function GenerateGame (a) {
+    const ScreenLobby = document.getElementById("Screen-Lobby");
+    const ScreenGame = document.getElementById("Screen-Game");
+    ScreenLobby.style.display = "none";
+    ScreenGame.style.display = "block";
+}
+export default GenerateGame;
--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
+import GenerateGame from "./Generate-Game.js"
 const ButtonGenerate = document.getElementById("generate-button");
-const ScreenMain = document.getElementById("Screen");
 
 ButtonGenerate.addEventListener("click", () => {
     const NumberOfCards = document.getElementById("input-number").value;
-    const numberOfCardsConverted = parseInt(NumberOfCards, 10);
-    if (!isNaN(numberOfCardsConverted)) {
-        if (numberOfCardsConverted % 2 === 0) {
-            alert("Generating cards...");
-        } else {
-            alert("Please enter an even number.");
-            return;
-        }
-    } else {
-        alert("Please enter a valid number.");
-        return
+    const NumbCard = parseInt(NumberOfCards, 10);
+    if (NumberOfCards === "") {
+        alert("Please enter a number.");
+        return;
     }
+    if (isNaN(NumbCard)) {
+        alert("Please enter a valid number.");
+        return;
+    }
+    if (NumbCard <= 0) {
+        alert("Please enter a number greater than zero.");
+        return;
+    }
+    if (!Number.isInteger(Number(NumbCard))) {
+        alert("Please enter an integer number.");
+        return;
+    }
+    if (NumbCard % 2 !== 0) {
+        alert("Please enter an even number.");
+        return;
+    }
+
+    GenerateGame(NumbCard);
 })
